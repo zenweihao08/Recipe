037ec3b62500428f4a17c4339e081045ad321613
Weihao <zenweihao08@gmail.com>
2020-05-24 18:37:06 +0800

    Grouped recipe features together into one module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,38 +6,32 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './shared/shopping-list.service';
 import { RecipeService } from './shared/recipe.service';
-import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 import { AuthComponent } from './auth/auth.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './alert/alert.component';
-import { RecipeDetailDefaultComponent } from './recipe/recipe-detail-default/recipe-detail-default.component';
+import { RecipeModule } from './recipe/recipe.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipeComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    RecipeEditComponent,
     AuthComponent,
     AlertComponent,
-    RecipeDetailDefaultComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RecipeModule
   ],
   providers: [ShoppingListService,RecipeService,{provide:HTTP_INTERCEPTORS,useClass:AuthInterceptorService,multi:true}],
   bootstrap: [AppComponent]
--- src/app/recipe/recipe.module.ts
+++ src/app/recipe/recipe.module.ts
@@ -1,11 +1,36 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { RecipeComponent } from './recipe.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeDetailDefaultComponent } from './recipe-detail-default/recipe-detail-default.component';
 
 
 
 @NgModule({
-    imports: [],
-    exports: [],
-    declarations: [],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        RouterModule
+    ],
+    exports: [
+        RecipeComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeEditComponent,
+        RecipeDetailDefaultComponent
+    ],
+    declarations: [
+        RecipeComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeEditComponent,
+        RecipeDetailDefaultComponent
+    ],
     providers: [],
 })
 export class RecipeModule { }
